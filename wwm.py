12a6796db502cbff1cd54e3fd222904b0e6d0656
electric-otter <krw122081@gmail.com>
2024-12-27 10:53:21 -0500

    Update wwm.py

--- wwm.py
+++ wwm.py
@@ -39,4 +39,72 @@ class WindowManager:
         # An array of windows
         self.windows = []
         self.current_window = 0
+        
+
+def run(self):
+    """
+    Setup and run window manager. This includes checking if another window manager is
+    running, listening for certain key presses, and handling events.
+    """
+
+    # Tell X server which events we wish to receive for the root window.
+    cookie = self.conn.core.ChangeWindowAttributesChecked(
+        self.root_window,
+        xcffib.xproto.CW.EventMask,  # Window attribute to set which events we want
+        [
+            # We want to receive any substructure changes. This includes window
+            # creation/deletion, resizes, etc.
+            xcffib.xproto.EventMask.SubstructureNotify |
+            # We want X server to redirect children substructure notifications to the
+            # root window. Our window manager then processes these notifications.
+            # Only a single X client can use SubstructureRedirect at a time.
+            # This means if the request to changes attributes fails, another window manager
+            # is probably running.
+            xcffib.xproto.EventMask.SubstructureRedirect,
+        ]
+    )
+
+    # Check if request was valid
+    try:
+        cookie.check()
+    except:
+        logging.error(logging.traceback.format_exc())
+        print('Is another window manager running?')
+        exit()
+
+    # Loop through actions listed in config and grab keys. This means we
+    # will get a KeyPressEvent if the key combination is pressed.
+    for action in self.config['actions']:
+        # Get keycode from string
+        keycode = self.key_util.get_keycode(
+            KeyUtil.string_to_keysym(action['key'])
+        )
+
+        # Get modifier from string
+        modifier = getattr(xcffib.xproto.KeyButMask, self.config['modifier'], 0)
+
+        self.conn.core.GrabKeyChecked(
+            # We want owner_events to be false so all key events are sent to root window
+            False,
+            self.root_window,
+            modifier,
+            keycode,
+            xcffib.xproto.GrabMode.Async,
+            xcffib.xproto.GrabMode.Async
+        ).check()
+
+    while True:
+        event = self.conn.wait_for_event()
+
+        if isinstance(event, xcffib.xproto.KeyPressEvent):
+            self._handle_key_press_event(event)
+        if isinstance(event, xcffib.xproto.MapRequestEvent):
+            self._handle_map_request_event(event)
+        if isinstance(event, xcffib.xproto.ConfigureRequestEvent):
+            self._handle_configure_request_event(event)
+
+        # Flush requests to send to X server
+        self.conn.flush()
+
+
 
